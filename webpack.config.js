5878724573993248faa792136e69b75c7d417b55
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-10 16:51:28 -0600

    Remove AngularJS, remove bower, webpack shenanigans

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "src/dist/bundle.css",
+    disable: process.env.NODE_ENV = "development"
+});
+
 module.exports = {
-    entry: "./src/app/main.ts",
-    output: {
-        filename: "src/dist/bundle.js"
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
-    module: {
-        loaders: [
-            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    },
-    plugins: [
-        new webpack.ContextReplacementPlugin(
-            // if you have anymore problems tweet me at @gdi2290
-            // The (\\|\/) piece accounts for path separators for Windows and MacOS
-            /(.+)?angular(\\|\/)core(.+)?/,
-            path.join(__dirname, 'src'), // location of your src
-            {} // a map of your routes 
-        )
-    ]    
+  entry: ["./src/app/main.ts", "main.scss"],
+  output: {
+    filename: "src/dist/bundle.js",
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    loaders: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+            use: [{
+                loader: 'css-loader'
+            }, {
+                loader: 'sass-loader'
+            }],
+            fallback: "style-loader"
+        })
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      // if you have anymore problems tweet me at @gdi2290
+      // The (\\|\/) piece accounts for path separators for Windows and MacOS
+      /(.+)?angular(\\|\/)core(.+)?/,
+      path.join(__dirname, "src"), // location of your src
+      {} // a map of your routes
+    ),
+  ],
 };
